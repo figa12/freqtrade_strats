848e0847822630bd5519b87342f877d57d49993d
Nicklas Andersen <figa@pop-os.localdomain>
2023-12-26 22:18:24 +0100

    update to notebook. Removed junk from zvwap strat

--- zvwap.py
+++ zvwap.py
@@ -60,7 +60,6 @@ class ZVWAPLBStrategy(IStrategy):
         var_sma = ta.SMA(var_pow, timeperiod=length)
         vwapsd = np.sqrt(var_sma)
         
-        #vwapsd = np.sqrt(dataframe['close'] - mean).pow(2).rolling(window=length)
         return (dataframe['close'] - mean) / vwapsd
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
@@ -82,48 +81,3 @@ class ZVWAPLBStrategy(IStrategy):
             'exit_long'] = 1
 
         return dataframe
-
-"""
-
-    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # Identify ZVWAP dips
-        zvwap_dipped = dataframe['zvwap'] <= self.params['buyLine']
-
-        # Generate buy signals
-        dataframe.loc[zvwap_dipped, 'buy_signal'] = 1
-        return dataframe
-
-
-    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # Generate sell signals
-        dataframe['sell_signal'] = 0
-        sell_condition = (dataframe['zvwap'] > self.params['sellLine']) & (dataframe['short_ema'] <= dataframe['long_ema'])
-        dataframe.loc[sell_condition, 'sell_signal'] = 1
-        return dataframe
-
-    def custom_stop_loss(self, pair: str, trade: 'Trade', current_time: 'datetime', current_rate: float, current_profit: float, **kwargs) -> float:
-        # Calculate stop loss based on percentage
-        return current_rate - (current_rate * self.params['stopLoss'] / 100)
-
-    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        zvwap_dipped = False
-
-        for i in range(1, 11):
-            zvwap_dipped = zvwap_dipped or (dataframe['zvwap'].shift(i) <= self.params['buyLine'])
-
-        dataframe['zvwapDipped'] = zvwap_dipped
-
-        # Generate buy signals
-        long_condition = (dataframe['short_ema'] > dataframe['long_ema']) & dataframe['zvwapDipped'] & qtpylib.crossed_above(dataframe['zvwap'], 0)
-        dataframe.loc[long_condition, 'buy_signal'] = 1
-        return dataframe
-
-    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # Generate sell signals
-        dataframe['sell_signal'] = 0
-
-        # Add sell conditions here based on your original script
-
-        return dataframe
-        
-"""
